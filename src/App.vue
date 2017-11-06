179e64a7fc3f31e8de259ed032677666d33abf16
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-11-05 20:26:53 -0600

    Add skeleton of countdown component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <div>
+      <h1 class="title is-size-1">Time until Christmas</h1>
+    </div>
+    <countdown-timer></countdown-timer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-};
+  import CountdownTimer from "./components/countdown-timer.vue";
+
+  export default {
+    name: 'App',
+    components: {
+      CountdownTimer
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  #app {
+    background: url();
+    
+  }
 </style>
---
+++ src/components/countdown-timer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <span>
+          58
+        </span>
+        <span>
+          days
+        </span>
+      </div>
+      <div class="column is-one-quarter">
+        <span>
+          12
+        </span>
+        <span>
+          hours
+        </span>
+      </div>
+      <div class="column is-one-quarter">
+        <span>
+          32
+        </span>
+        <span>
+          minutes
+        </span>
+      </div>
+      <div class="column is-one-quarter">
+        <span>
+          12
+        </span>
+        <span>
+          seconds
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Countdown-Timer'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
